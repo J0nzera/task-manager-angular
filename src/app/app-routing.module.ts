5b972311359c4f2a9b970c3d2d08c04fd5c7c734
Jon Silva <jonathasinv@gmail.com>
2020-06-14 20:39:19 -0300

    tarefas routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TarefaRoutes } from './tarefas/tarefas-routing.module';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/tarefas/listar',
+    pathMatch: 'full'
+  },
+  ...TarefaRoutes
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
