2ed4afc043819111ecf89b51e937ce03644c4694
Jon Silva <jonathasinv@gmail.com>
2020-06-15 17:44:18 -0300

    Update listar-tarefa.component.ts

--- src/app/tarefas/listar/listar-tarefa.component.ts
+++ src/app/tarefas/listar/listar-tarefa.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarefa } from '../shared/tarefa.model';
+import { TarefaService } from '../shared/tarefa.service';
 
 @Component({
   selector: 'app-listar-tarefa',
@@ -7,9 +9,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarTarefaComponent implements OnInit {
 
-  constructor() { }
+  tarefas: Tarefa[];
+
+  constructor(private tarefaService: TarefaService) { }
 
   ngOnInit(): void {
+    this.tarefas = this.listarTodos();
+  }
+
+  listarTodos(): Tarefa[] {
+    return this.tarefaService.listarTodos();
+  }
+
+  remover($event: any, tarefa: Tarefa): void {
+    $event.preventDefault();
+    if (confirm('Deseja remover a tarefa "' + tarefa.nome + '"?')) {
+      this.tarefaService.remover(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
+  }
+
+  alterarStatus(tarefa: Tarefa): void {
+    if (confirm('Deseja alterar o status da tarefa "' + tarefa.nome + '"?')){
+      this.tarefaService.alterarStatus(tarefa.id);
+      this.tarefas = this.listarTodos();
+    }
   }
 
 }
